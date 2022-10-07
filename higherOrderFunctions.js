eaf3ff7dcf7d6f0c1bd48db2ddcd8f3c5e62152e
AatirNadim <aatir.nadim@gmail.com>
2022-10-07 12:41:42 +0000

    ...

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -34,7 +34,7 @@ const countries = [
 countries.filter((itr) => {return !itr.includes('land')}).forEach((itr) => console.log(itr.toUpperCase()))
 
 
-const ages = [24, 22, 25, 32, 35, 18]
+const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
 
 let val = ages.every((itr) => { return !(itr&1)})
 ages.sort((a, b) => {return})
@@ -42,3 +42,32 @@ console.log(!(24&1))
 
 console.log(ages.sort((a, b) => {return a - b}))
 console.log(ages)
+console.log(ages.sort((a, b) => {return b - a}))
+console.log(ages)
+const newarr = [...countries]
+
+console.log(newarr.sort((a, b) => {return a.length - b.length}))
+
+const users = [
+    { name: 'Asabeneh', age: 150 },
+    { name: 'Brook', age: 50 },
+    { name: 'Eyob', age: 100 },
+    { name: 'Elias', age: 22 },
+  ]
+console.log(users.sort((a, b) => {
+    if(a['age'] < b['age']) return -1;
+    if(a['age'] > b['age']) return 1;
+    return 0
+}))
+
+let sum = 0
+sum = ages.reduce((prev, itr) => {return prev + itr}, 0)
+console.log(Number.parseInt(sum/ages.length))
+console.log(Number.parseFloat('    +10.233'))
+
+const obj = {}
+obj.arr = [...ages]
+
+obj.sum = sum
+obj.length = ages.length
+obj.arr.forEach((itr) => console.log('value -->' + itr))
