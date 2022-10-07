7f716a267634e5c93f71eeece9fb39d4953e1973
AatirNadim <aatir.nadim@gmail.com>
2022-10-07 11:56:37 +0000

    higher order and sort

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -5,3 +5,40 @@ const cube = (n) => {return n*callback(n)}
 console.log(cube(21349))
 
 //returning function as value
+
+const onetier = (n) => {
+    const twotier = (m) => {
+        const threetier = (o) => {
+            return o*m*n;
+        }
+        return threetier
+    }
+    return twotier
+}
+
+console.log(onetier(2)(3)(4))
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+  ]
+
+countries.filter((itr) => {return !itr.includes('land')}).forEach((itr) => console.log(itr.toUpperCase()))
+
+
+const ages = [24, 22, 25, 32, 35, 18]
+
+let val = ages.every((itr) => { return !(itr&1)})
+ages.sort((a, b) => {return})
+console.log(!(24&1))
+
+console.log(ages.sort((a, b) => {return a - b}))
+console.log(ages)
