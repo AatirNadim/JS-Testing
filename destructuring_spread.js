e85f1ba8e6565ea2af87422d50b4ff3d3e62fe39
AatirNadim <aatir.nadim@gmail.com>
2022-10-09 07:10:03 +0000

    spread spreaded to full measure

---
+++ destructuring_spread.js
@@ -0,0 +1,31 @@
+//this workplace is to delve into the spread operations
+
+const numbers = [1, 2, 3,4]
+const [one, two, ...rest]= numbers
+console.log(one, two, rest)
+
+const arr = [[1, 2, 3, 4], [5, 6, 7,8]]
+const [thee, four] = arr
+console.log(thee, ',,', four)
+
+//skipping values
+
+let [et, un, tes] = numbers
+console.log(et, un, tes)
+
+let [p1 = 'juspay', p2, p3] = [undefined, 'tsunade', 'faasil', 'asss']
+console.log(p1, p2, p3)
+
+//destructuring objects
+
+const obj1 = {
+    name : 'aatir', class:'1st', age: 10, traits : {
+        behaviour : ['behaviour1', 'behaviour2']
+    }
+}
+console.log(obj1.traits.behaviour)
+console.log(obj1.class)
+
+//note that class is a keyword
+let {name, traits, age, class : c} = obj1
+console.log(name, traits, c)
