09a9807f3b1940bcbbc70e5291bade1a636ce853
AatirNadim <aatir.nadim@gmail.com>
2022-10-09 10:01:52 +0000

    more into regex

--- regex_matching.js
+++ regex_matching.js
@@ -24,3 +24,16 @@ console.log('I have 12443 matches pending wherein I require 233 more'.match(/[0-
 console.log('I have 12443 matches pending wherein I require 233 more'.match(/\d/g))//wprks the same as above
 console.log('I have 12443 matches pending wherein I require 233 more'.match(/\d+/g))
 // console.log(txt.replace(/[A-Z]|*thon/g, 'jojo'))
+console.log('I have 12443 matches pending wherein I require 233 more'.match(/\d../g))
+//. represents any character except newline
+console.log('I have 12443 matches pending wherein I require 233 more'.match(/\d.+/g))
+console.log('I have \n12443 matches pending \nwherein I requi\nre 233 more'.match(/.+/g))
+//above regex instruction, splits the given string into groups with the delimiter as '\n'
+console.log('abcdefghijklmnopqrstuvwxyz'.match(/[ax](bc|yz)+/g))
+
+console.log('Apples are apples'.match(/(A|a)pples/g))
+console.log('Apples are apples'.match(/(A|a).{3}/g))
+//both above and below work the same
+console.log('Apples are apples'.match(/[Aa].{3}/g))
+// console.log('')
+
