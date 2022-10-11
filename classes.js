32b73d3ae69294f3aac4711535661aa3c3280d40
AatirNadim <aatir.nadim@gmail.com>
2022-10-11 17:00:43 +0000

    getters and setters

--- classes.js
+++ classes.js
@@ -5,7 +5,7 @@ class class1 {
         this.name= name
         this.age = age
         this.id = id
-
+        this.arr = []
     }
     disp() {
         console.log(`name --> ${this.name}, age --> ${this.age}, id --> ${this.id}`)
@@ -18,3 +18,47 @@ obj.disp()
 const obj1 = new class1('hill', 23, 'yy03ed')
 console.log(obj)
 console.log(obj1)
+
+//this works just fine
+obj.yips = true
+console.log(obj)
+
+for(let i=0;i<12;i++) obj1.arr.push(i*34)
+
+console.log(obj1)
+
+//getter functions
+
+class Person {
+    constructor(firstName, lastName, age, country, city) {
+      this.firstName = firstName
+      this.lastName = lastName
+      this.age = age
+      this.country = country
+      this.city = city
+      this.score = 0
+      this.skills = []
+    }
+
+    getFullName() {
+        return this.firstName + ' '+ this.lastName
+    }
+    get getName() {
+        return this.firstName + ' '+ this.lastName
+    }
+    set setCity (name) {
+        this.city = name
+    }
+}
+
+const person1 = new Person('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+//both below work the same
+console.log(person1.getFullName())
+console.log(person1.getName)
+
+person1.setCity = 'Lucknow'
+console.log(person1.city)
+
+//setter functions
+
+console.log(person1)
