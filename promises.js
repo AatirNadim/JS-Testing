763c6ea1f9235e662af289676caf1a58d420a7f1
AatirNadim <aatir.nadim@gmail.com>
2022-10-13 13:55:38 +0000

    promises-begin

---
+++ promises.js
@@ -0,0 +1,50 @@
+console.log('getting started with promises')
+setTimeout(() => {
+    console.log('afer waiting some time')
+}, 3000)
+// alias to the functioning of a promise
+
+const func = (callback) => {
+    setTimeout(() => {
+        const arr = [1, 2,3, 4,5]
+        callback(null, 'rejected message') //can be the arr or 0 or any value
+    }, 3000)
+}
+
+const callback = (arr, msg) => {
+    if(arr) console.log(...arr)
+    else console.log(msg)
+}
+
+func(callback)
+
+//moving on to promises
+const func1 = (arr) => {
+    let b= true
+    arr.forEach((itr) => {
+        console.log('------- ' + itr)
+        if(itr > 20) b = false
+    })
+    arr.forEach((itr) => console.log(itr))
+    console.log('everything fine')
+    return b
+}
+const pro = new Promise((resolve, reject) => {
+    const arr =[1, 2, 3,4,5, 26]
+    const val = func1(arr)
+    console.log('value returned is => ' + val)
+    if(val) resolve('pending matter resolved')
+    else reject('pending matter rejected')
+}) //resolve, reject are the callback functions
+
+pro
+.then((msg) => {console.log(msg)})
+.catch((msg) => {console.error(msg)})
+
+
+const promise = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+        const arr = [1, 2, 3, 4, 5, 6]
+
+    },3000)
+})
